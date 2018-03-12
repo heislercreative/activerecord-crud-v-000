36dab631b47fb1cf2a230b23a5dcb8ad3287d239
Brennan Heisler <learn@heislercreative.com>
2018-03-12 00:58:40 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -53,7 +53,10 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  
+  Movie.find_each do |m|
+    
+  end
+
 end
 
 def can_be_found_updated_and_saved
