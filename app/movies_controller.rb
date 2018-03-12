fe81b6721ba64604e15961a8309285e9933fae9d
Brennan Heisler <learn@heislercreative.com>
2018-03-12 00:50:13 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,8 +31,7 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  movie = Movie.all.first
-  movie.name
+  Movie.all.first.title
 end
 
 def can_get_the_last_item_in_the_database
